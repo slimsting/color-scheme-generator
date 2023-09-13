f6e4eaeaf34dc0666e598a81b477117311418dea
slimsting <newbon.gadson@gmail.com>
2023-09-13 23:33:47 +0300

    final
    completed

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const colorSelectionForm = document.getElementById("form");
+const colorSelectionForm = document.getElementById("form"); // grab the input form
+const colorsArr = []; // global variable to hold the colors retreived from the API for easy access anywhere in the program
 
 colorSelectionForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+  // listen for submit
+  e.preventDefault(); // prevent default for behaviour
 
   const formData = new FormData(colorSelectionForm); //capture form data
-  const inputColor = formData.get("input-color").substring(1); // subtring(1) used to begin the string from index 1
+  const inputColor = formData.get("input-color").substring(1); // subtring(1) used to begin the string from index 1 (to remove the initial '#' from the hex value captured)
   const colorMode = formData.get("color-mode");
-
-  // const colorValue = document
-  //   .getElementById("color-selector")
-  //   .value.substring(1);
-  // const colorScheme = document.getElementById("color-scheme").value;
-
   const link = `https://www.thecolorapi.com/scheme?hex=${inputColor}&mode=${colorMode}`; //create a link with a query string to retreive the desired color scheme
 
   fetch(link)
@@ -25,12 +21,36 @@ colorSelectionForm.addEventListener("submit", (e) => {
 
       renderScheme(colors); // render the color scheme to the DOM
       renderNames(colors); // render the names of the colors to the DOM
+      colorsArr = colors;
     });
 });
 
-//listen for clicks on the DOM the details of the color clicked on
+//listen for clicks on the DOM to identify which color was clicked
 document.addEventListener("click", function (e) {
   document.execCommand("copy");
+
+  const clickedElementID = e.target.id; // retreive the ID of the clicked element
+
+  if (
+    clickedElementID === "color-1" ||
+    clickedElementID === "color-2" ||
+    clickedElementID === "color-3" ||
+    clickedElementID === "color-4" ||
+    clickedElementID === "color-5" // if it is one of the colors
+  ) {
+    const colorClicked = document.getElementById(clickedElementID).innerText; // store the value of the clicked color for later
+
+    document.getElementById(
+      clickedElementID
+    ).innerHTML = `<p id="copy-alert" style="color: red; font-weight: bold;">Copied!<p>`; // display the word copied to show the user they have copied the color value
+
+    const myTimeout = setTimeout(restoreColorValue, 2000);
+    function restoreColorValue() {
+      document.getElementById(
+        clickedElementID
+      ).innerHTML = `<p id="copy-alert">${colorClicked}<p>`; // restore the color value from before to the DOM after 2 seconds
+    }
+  }
 });
 
 //function to render the colors retreived from the API based on the seed color
